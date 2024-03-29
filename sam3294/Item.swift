a01312a0759d8ba8bfdf19627a3743f5d1266984
userno1 <wplover90@gmail.com>
2024-03-29 12:08:40 +0900

    Initial Commit

---
+++ sam3294/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  sam3294
+//
+//  Created by userno1 on 2024/03/29.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ sam3294UITests/sam3294UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sam3294UITestsLaunchTests.swift
+//  sam3294UITests
+//
+//  Created by userno1 on 2024/03/29.
+//
+
+import XCTest
+
+final class sam3294UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
